cd32e47a52fce9dd13e97d9c205953b5e57be1a5
Tino Kallinich <tkallinich-friedrich@inovex.de>
2021-03-08 08:44:56 +0100

    gernerated build files from schema

---
+++ lib/graphql_api.g.dart
@@ -0,0 +1,40 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'graphql_api.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+EmployeeData _$EmployeeDataFromJson(Map<String, dynamic> json) {
+  return EmployeeData()
+    ..employee = json['employee'] == null
+        ? null
+        : Employee.fromJson(json['employee'] as Map<String, dynamic>);
+}
+
+Map<String, dynamic> _$EmployeeDataToJson(EmployeeData instance) =>
+    <String, dynamic>{
+      'employee': instance.employee?.toJson(),
+    };
+
+Employee _$EmployeeFromJson(Map<String, dynamic> json) {
+  return Employee()..id = json['id'] as String;
+}
+
+Map<String, dynamic> _$EmployeeToJson(Employee instance) => <String, dynamic>{
+      'id': instance.id,
+    };
+
+EmployeeDataArguments _$EmployeeDataArgumentsFromJson(
+    Map<String, dynamic> json) {
+  return EmployeeDataArguments(
+    id: json['id'] as String,
+  );
+}
+
+Map<String, dynamic> _$EmployeeDataArgumentsToJson(
+        EmployeeDataArguments instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+    };
